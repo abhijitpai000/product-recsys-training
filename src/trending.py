5420e3967b4d3e3d3c81a63ee29e76fd7cc5824b
unknown <abhijitpai000@gmail.com>
2020-10-30 12:59:36 +0530

    Add demographic filtering module

---
+++ src/trending.py
@@ -0,0 +1,52 @@
+"""
+Computes Weighted Average for each products.
+
+Formula:
+WR = (v/v+m)*R+(m/v+m)*C
+
+v = Total Reviews
+m = Minimum votes required to qualify to trending list
+R = Average Rating
+C = Mean rating across the report.
+"""
+import pandas as pd
+from pathlib import Path
+
+
+def top_trending():
+    """
+    Computes Weighted Average for each products.
+
+    Formula:
+    WR = (v/v+m)*R+(m/v+m)*C
+
+    v = Total Reviews
+    m = Minimum votes required to qualify to trending list
+    R = Average Rating
+    C = Mean rating across the report.
+
+    Returns
+    -------
+        Top Trending Products Categories.
+    """
+    review_data = pd.read_csv("datasets/review_data.csv")
+
+    trending = review_data.groupby("product_category")[["review_score"]].agg(["mean", "count"])
+    trending.columns = ["avg_review_score", "total_reviews"]
+
+    # Computing C.
+    c = trending["avg_review_score"].mean()
+
+    # Computing m, a product should have votes > 90% of items.
+    m = trending["total_reviews"].quantile(0.9)
+
+    # Weighted Average.
+    trending["weighted_average"] = (trending["total_reviews"]/(trending["total_reviews"]+m))*trending["avg_review_score"] + \
+                               (m/(trending["total_reviews"]+m))*c
+
+    # Top 10.
+    top_trending = trending.sort_values("weighted_average", ascending=False).head(10)
+    file_path = Path.cwd() / "datasets/top_trending.csv"
+    top_trending.to_csv(file_path, index=False)
+
+    return top_trending

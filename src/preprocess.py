766f6fb40f13aec2035e30ae7431f2c6e0f05a57
unknown <abhijitpai000@gmail.com>
2020-11-05 23:17:47 +0530

    Remove Encoding func

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,9 +4,7 @@ Performs data pre-processing.
 - Encodes "Customer ID" and "Product Category"
 """
 import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder
 from pathlib import Path
-import joblib
 
 
 def _combine_datasets():
@@ -47,55 +45,28 @@ def _combine_datasets():
     # Review data raw
     review_data_raw = data[["customer_unique_id", "product_category_name_english", "review_score"]].copy()
     review_data_raw["product_category_name_english"].fillna("Others", axis=0, inplace=True)  # Fix Missing categories
+
+    # Re-naming columns
+    review_data_raw.rename(columns={"customer_unique_id": "customer_id",
+                                    "product_category_name_english": "product_category"},
+                           inplace=True)
     return review_data_raw
-
-
-def _encode_categorical(review_data_raw):
-    """
-    Encoding Customer ID and Product Category.
-
-    Parameters
-    ----------
-        review_data_raw: pandas dataframe.
-
-    Yields
-    ------
-        ord_encoder.pkl in "models"
-
-    Returns
-    -------
-        review_data pandas dataframe
-    """
-    ord_encoder = OrdinalEncoder()
-    cat_cols = ["customer_unique_id", "product_category_name_english"]
-    encoded = pd.DataFrame(ord_encoder.fit_transform(review_data_raw[cat_cols]),
-                           columns=["customer_id", "product_category"],
-                           index=review_data_raw[cat_cols].index)
-
-    review_data = encoded.join(review_data_raw["review_score"])
-
-    file_path = Path.cwd() / "datasets/review_data.csv"
-    review_data.to_csv(file_path, index=False)
-
-    encoder_path = Path.cwd() / "models/ord_encoder.pkl"
-    joblib.dump(ord_encoder, encoder_path)
-    return review_data
 
 
 def make_dataset():
     """
     Performs pre-processing
     - Combines datasets
-    - Encodes categorical features
 
     Yields
     ------
         review_data.csv in "datasets"
-        encoder.pkl in "models"
     Returns
     -------
         review_data
     """
-    review_data_raw = _combine_datasets()
-    review_data = _encode_categorical(review_data_raw=review_data_raw)
+    review_data = _combine_datasets()
+
+    file_path = Path.cwd() / "datasets/review_data.csv"
+    review_data.to_csv(file_path, index=False)
     return review_data

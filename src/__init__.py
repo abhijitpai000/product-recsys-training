836f5429e4d35975ee30e2c1f1ab85b32cfbba93
unknown <abhijitpai000@gmail.com>
2020-11-15 13:52:08 +0530

    Add preprocess import

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,12 @@
 """Restricting access to private modules."""
 
+from src.preprocess import make_datasets
 from src.trending import compute_top_ten
 from src.item_rec_sys import train_item_rec_sys
 from src.user_rec_sys import train_user_rec_sys
 
-__all__ = ["compute_top_ten",
+
+__all__ = ["make_datasets",
+           "compute_top_ten",
            "train_user_rec_sys",
            "train_item_rec_sys"]

c97be23ad3d06678554d382bd2e4114cbd40e1a5
unknown <abhijitpai000@gmail.com>
2020-11-15 13:53:14 +0530

    Add preprocess import

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 """Generate Trained algorithms through command line."""
 
+from src.preprocess import make_datasets
 from src.trending import compute_top_ten
 from src.item_rec_sys import train_item_rec_sys
 from src.user_rec_sys import train_user_rec_sys
 
 
 if __name__ == '__main__':
+    make_datasets()
     compute_top_ten()
     train_item_rec_sys()
     train_user_rec_sys()
